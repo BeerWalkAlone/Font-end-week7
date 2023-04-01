ea42932135a25643a1b183b3297627417252a7bf
BeerWalkAlone <sinnakhon@hotmail.com>
2023-04-01 15:22:25 +0700

    wow

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Card, Container, TextField, Button } from '@mui/material/'
 import { useNavigate } from "react-router-dom";
 import SendIcon from '@mui/icons-material/Send';
@@ -7,24 +7,47 @@ import Avatar from '@mui/material/Avatar';
 import ImF from './Images/facebook.png'
 import ImG from './Images/ig.png'
 import ImC from './Images/3600912.png'
+import axios from 'axios'
 
 export default function App() {
-    const navigate = useNavigate();
-    const handleNvigate = (path) => {
-        navigate(path);
-    };
+    const navigation = useNavigate()
 
+    const [email, setemail] =useState("")
+    const [password, setpassword] = useState("")
 
+    const handleLogin = () => {
+        axios ({
+            url:'http://localhost:3000/Login',
+            method:'post',
+            data:{
+                email: email,
+                password: password
+            }
+        }).then(res => {
+            switch (res.data.status) {
+                case 200:
+                    alert(res.data.msg)  
+                    navigation('/profile')             
+                    break;
+                case 404:
+                    alert(res.data.msg)               
+                    break;          
+                default:
+                    alert('Login failed')
+                    break;
+            }
+        })
+    }
 
 
     return (
-        <Box sx={{
+        <Box sx={{ 
             background: '#2980b9',
             background: 'linear-gradient(to right, #2980b9, #6dd5fa, #ffffff)',
             background: 'linear-gradient(to right, #2980b9, #6dd5fa, #ffffff)',
-            height: '100vh'
+            height: '100%'
         }}>
-            <Container maxWidth='lg' sx={{ paddingTop: 10, marginLeft: 85 }}>
+            <Container  sx={{ paddingTop: 10, marginLeft: 85 }}>
                 <Card sx={{ height: 650, width: 450 }}>
                     <Box sx={{
                         background: ' #C9D6FF',
@@ -42,13 +65,15 @@ export default function App() {
                                 paddingLeft: 5,
                                 paddingTop: 5
                             }}>
-                            <TextField fullWidth label="Email" id="fullWidth" variant="standard" />
+                            <TextField fullWidth label="Email" id="fullWidth" variant="standard"
+                            onChange={(e) => setemail(e.target.value)} />
                             <br /> <br /> <br /> <br />
-                            <TextField fullWidth label="Password" id="fullWidth" variant="standard" type="password"/>
+                            <TextField fullWidth label="Password" id="fullWidth" variant="standard" type="password"
+                            onChange={(e) => setpassword(e.target.value)}/>
                         </Box>
 
                         <Stack direction="row" spacing={2} paddingTop={6} paddingLeft={10} width={300}>
-                            <Button variant="contained" endIcon={<SendIcon />} fullWidth onClick={() => handleNvigate("/Profile")}
+                            <Button variant="contained" endIcon={<SendIcon />} fullWidth onClick={handleLogin}
                                 sx={{
                                     background: '#ff4b1f',
                                     background: 'linear-gradient(to right, #1fddff, #ff4b1f)',
--- src/profile.jsx
+++ src/profile.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState}from 'react'
 import { Avatar, Container, Box, Typography, TextField, MenuItem } from '@mui/material'
 import Img from './Images/smile.png'
+import axios from 'axios'
+
 export default function Profile() {
-  const data = [
-    { id: 1, title: 'Username', lable: 'username' },
-    { id: 1, title: 'Email', lable: 'email' },
-    { id: 1, title: 'Password', lable: 'password' },
-    { id: 1, title: 'Firstname', lable: 'firstname' },
-    { id: 1, title: 'Lastname', lable: 'lastname' },
-  ]
+const [userInfo, setuserInfo] = useState([])
+useEffect(() => {
+  axios({
+    url:'http://localhost:3000/user/5',
+    method:'get'
+    
+  }).then(res => {
+    
+    setuserInfo(
+     [
+      { id: 1, title: 'Username', lable: 'username', value: res.data[0].username },
+      { id: 1, title: 'Email', lable: 'email' , value: res.data[0].email},
+      { id: 1, title: 'Password', lable: 'password' , value: res.data[0].password},
+      { id: 1, title: 'Firstname', lable: 'firstname' , value: res.data[0].firstname},
+      { id: 1, title: 'Lastname', lable: 'lastname', value: res.data[0].lastname},
+    ])
+  })
+}, [])
+
+
+
+
+ 
   const language = [
     {
       value: 'Lao',
@@ -29,7 +47,7 @@ export default function Profile() {
   ]
 
   return (
-    <Container maxWidth='xl'>
+    <Container maxWidth='lg'>
       <Box display='flex'>
         <Box flex={1} paddingTop={3}>
           <Avatar sx={{ width: 250, height: 200, fontSize: 60, marginLeft: 3 }}>
@@ -46,13 +64,13 @@ export default function Profile() {
       <Box paddingTop={10}>
         <Typography variant='h4'>Account</Typography>
 
-        {data.map(rows =>
+        {userInfo.map(rows =>
           <Box display={'flex'} alignItems='center' p={2}>
             <Box flex={1}>
-              <Typography variant='h6'>{rows.title}</Typography>
+              <Typography variant='h6' >{rows.title}</Typography>
             </Box>
             <Box flex={2}>
-            <TextField fullWidth  variant='outlined' label={rows.lable} value={''} />
+            <TextField fullWidth  variant='outlined' label={rows.lable} value={rows.value || ""} />
             </Box>
           </Box>
         )}
